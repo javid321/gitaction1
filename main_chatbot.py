9ebb19c7c08da0a47cf20574d6ee08f37dcccb01
Javid <akku.javid@gmail.com>
2023-08-29 22:18:53 +0700

    Update main_chatbot.py

--- main_chatbot.py
+++ main_chatbot.py
@@ -15,7 +15,7 @@ intents = json.loads(open("./intents.json").read())
 intents_df = pd.read_json("./intents.json")
 
 # Flatten data
-df_chat = pd.json_normalize(intents, record_path = ["intents"])
+df_chat = pd.json_normalize(intents, record_path=["intents"])
 df_chat = df_chat[["tag", "patterns"]]
 df_chat = df_chat.explode(["patterns"], ignore_index=True)
 df_chat = pd.DataFrame(df_chat)
@@ -28,18 +28,17 @@ response_list = []
 
 
 def remove_unused(text):
-    text = re.sub("[0-9]+", "", text) # untuk menghilangkan angka
-    #untuk menghilangkan non-ASCII characters dan unicode
+    text = re.sub("[0-9]+", "", text)  # untuk menghilangkan angka
+    # untuk menghilangkan non-ASCII characters dan unicode
     text = text.encode("ascii", "ignore").decode("utf-8") 
     text = re.sub(r"[^\x00-\x7f]", r"", text)  
-    text = re.sub(r"[^\w]", " ", text) # untuk menghiilangkan selain alpha numerik
+    text = re.sub(r"[^\w]", " ", text)  # untuk menghiilangkan selain alpha numerik
     # untuk menghilangkan double atau lebih spasi
     space = ["    ", "   ", "  "]
     for i in space:
         text = text.replace(i, " ")
     text = text.lower().strip()
     # text = text.translate(str.maketrans("", "", string.punctuation))
-    
     return text
 
 
@@ -47,16 +46,14 @@ def lemm(word):
     words = tokenizer(word)
     words = [lemmatizer.lemmatize(text) for text in words]
     words = [" ".join(t for t in words)]
-    
     return words
 
 
 def bagofword(text):
-    words= remove_unused(text)
+    words = remove_unused(text)
     words = lemm(words)
     # print("words : {}".format(words))
     bag = vectorizer_bow.transform(words)
-    
     return bag.toarray()
 
 
@@ -64,7 +61,7 @@ def prediction(text):
     error_treshold = 0.70
     if text == "":
         result_index = "empty"
-    else :
+    else:
         bow = bagofword(text)
         result = model.predict(bow)[0]
         # print("result : {}".format(result))
@@ -73,16 +70,22 @@ def prediction(text):
             result_index = np.argmax(result)
         else:
             result_index = "unknown"
-    
     return result_index
 
 
 def get_response(res_index, intent_json):
     # print("clss : {}".format(classes))
-    if res_index=="unknown":
+    if res_index == "unknown":
         result = "I\"m sorry, I didnt get it"
-    elif res_index=="empty":
-        result = random.choice(["what?, type something!", "try to type something!", "Hello?", "you didnt type anything yet!"])
+    elif res_index == "empty":
+        result = random.choice(
+            [
+                "what?, type something!",
+                "try to type something!",
+                "Hello?",
+                "you didnt type anything yet!"
+            ]
+        )
     else:
         tag = classes[res_index]
         # print("tag : {}".format(tag))
@@ -90,8 +93,7 @@ def get_response(res_index, intent_json):
         for i in list_of_intent:
             if i["tag"] == tag:
                 result = random.choice(i["responses"])
-                break
-                
+                break      
     return result
 
 
@@ -102,7 +104,6 @@ app = Flask(__name__, template_folder="template", static_folder="template")
 def home():
     global response_list
     response_list = []
-    
     return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
@@ -112,7 +113,6 @@ def predict():
     print("pred : {}".format(ints))
     response = get_response(ints, intents)
     response_list.append([msg, response])
-    
     return render_template("index.html", response=response_list)
 
 if __name__ == "__main__":
